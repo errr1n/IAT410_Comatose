c0294b7367d248cae503614278ef4bb48fbbe32d
errr1n <eteply@telus.net>
2024-01-21 20:03:50 -0800

    added some teleport scripts

--- IAT410_ComatoseGame/Assets/Scripts/FixedTeleportation.cs
+++ IAT410_ComatoseGame/Assets/Scripts/FixedTeleportation.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class FixedTeleportation : MonoBehaviour
 {
+
+    Camera cam; 
+    TeleportationSpot teleportationSpot;
+    public LayerMask teleportMask;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity, teleportMask))
+        {
+            if(hit.collider.GetComponent<TeleportationSpot>()!= null)
+            {
+                teleportationSpot = hit.collider.GetComponent<TeleportationSpot>();
+                teleportationSpot.m_Renderer.enabled = true;
+
+                if(Input.GetMouseButtonDown(0))
+                {
+                    transform.position = teleportationSpot.target.position;
+                }
+            }
+        }
     }
 }
