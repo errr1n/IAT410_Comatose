2d757efa4153a021d6ec4538e1294f9faab4f285
errr1n <eteply@telus.net>
2024-01-28 20:39:59 -0800

    text appears and dissapears

--- IAT410_ComatoseGame/Assets/Scripts/Text.cs
+++ IAT410_ComatoseGame/Assets/Scripts/Text.cs
@@ -13,6 +13,9 @@ public class Text : MonoBehaviour
     // create a trigger game object
     public GameObject trigger;
 
+    // adjustable float to change delay of disapearance
+    [SerializeField] float delay = 1f;
+
     // by default, the text is not visible
     void Start()
     {
@@ -28,24 +31,64 @@ public class Text : MonoBehaviour
         {
             // sets visibilty of UI text object to true
             UIObject.SetActive(true);
+
+            // starts coroutine (timer)
+            StartCoroutine(Delay(delay));
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    // coroutine (timer) funtion which takes in temporary variable
+    IEnumerator Delay(float _delay)
     {
-        
-    }
-
-    // upon exiting the trigger
-    void OnTriggerExit(Collider other)
-    {
-        // sets visibilty of UI text object to false 
-
-        //(HERE)
-        // UIObject.SetActive(false);
-
-        // destorys the trigger so text only appears once
+        // coroutine yield and waits for specified number of seconds
+        yield return new WaitForSecondsRealtime(_delay);
+        // sets visibilty of UI text object to false
+        UIObject.SetActive(false);
+        // destroys trigger (can only play once)
         Destroy(trigger);
     }
 }
+
+// for future reference of erins stupidity
+
+// // Update is called once per frame
+//     void Update()
+//     {
+//         // if(timerStarted == true)
+//         // {
+//             // Debug.Log(timerStarted);
+//             // StartCoroutine(Delay());
+//             // timerStarted = false;
+//             // Debug.Log(timerStarted);
+//         // }
+
+//         // Debug.Log(timerStarted);
+//     }
+
+    // void awake()
+    // {
+    //     // StartCoroutine(Delay());
+    //     Debug.Log("Awake");
+    // }
+
+    // upon exiting the trigger
+    // void OnTriggerExit(Collider other)
+    // {
+    //     // sets visibilty of UI text object to false 
+
+    //     //(HERE)
+    //     // UIObject.SetActive(false);
+    //     // removeText();
+    //     // Invoke("removeText", 3);
+
+    //     // destorys the trigger so text only appears once
+    //     Destroy(trigger);
+    // }
+
+    // ERIN's edits
+    // void removeText()
+    // {
+        // sets visibilty of UI text object to false 
+        // UIObject.SetActive(false);
+    // }
+
