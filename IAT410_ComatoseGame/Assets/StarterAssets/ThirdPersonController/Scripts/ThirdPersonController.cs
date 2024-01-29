712947733469c131345c71733e1a44840456b7de
errr1n <eteply@telus.net>
2024-01-29 11:55:00 -0800

    crosshair and right click zoom functionality

--- IAT410_ComatoseGame/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ IAT410_ComatoseGame/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -28,6 +28,9 @@ namespace StarterAssets
         [Tooltip("Acceleration and deceleration")]
         public float SpeedChangeRate = 10.0f;
 
+        // zoom aim sensitivity - Erin
+        public float Sensitivity = 1f;
+
         public AudioClip LandingAudioClip;
         public AudioClip[] FootstepAudioClips;
         [Range(0, 1)] public float FootstepAudioVolume = 0.5f;
@@ -198,8 +201,8 @@ namespace StarterAssets
                 //Don't multiply mouse input by Time.deltaTime;
                 float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
 
-                _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier;
-                _cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier;
+                _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier * Sensitivity;
+                _cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier * Sensitivity;
             }
 
             // clamp our rotations so our values are limited 360 degrees
@@ -387,6 +390,12 @@ namespace StarterAssets
             {
                 AudioSource.PlayClipAtPoint(LandingAudioClip, transform.TransformPoint(_controller.center), FootstepAudioVolume);
             }
+        }
+
+        //Erin look sensitivity
+        public void SetSensitivity(float newSensitivity)
+        {
+            Sensitivity = newSensitivity;
         }
     }
 }
