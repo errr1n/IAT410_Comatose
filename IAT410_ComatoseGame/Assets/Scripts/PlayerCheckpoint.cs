844674ce879ce703224ddec042cfc3576d56faba
Anika Richards <109831070+anika-26@users.noreply.github.com>
2024-02-06 23:30:06 -0800

    checkpoints

--- IAT410_ComatoseGame/Assets/Scripts/PlayerCheckpoint.cs
+++ IAT410_ComatoseGame/Assets/Scripts/PlayerCheckpoint.cs
@@ -4,32 +4,30 @@ using UnityEngine;
 
 public class PlayerCheckPoint : MonoBehaviour
 {
-    public GameObject player;
-    Vector3 spawnPoint;
-    // Start is called before the first frame update
-    void Start()
-    {
-        //setting spawn point tobe original position of player
-        spawnPoint = player.transform.position;
-    }
+    [SerializeField] private GameObject player;
+    [SerializeField] private  List<GameObject> checkPoints;
+    [SerializeField] private Vector3 spawnPoint;
+  
 
     // Update is called once per frame
     void Update()
     {
-        if(player.GetComponent<PlayerHealth>().checkHealth() <= 90){
+        if(player.GetComponent<PlayerHealth>().checkHealth() <= 100){
        
             player.transform.position = spawnPoint;
+            gameObject.GetComponent<PlayerHealth>().respawn();
            
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        if(other.gameObject.CompareTag("Checkpoint"))
         {
             Debug.Log("Checkpoint");
             
             spawnPoint = other.transform.position;
+            Destroy(other.gameObject);
             //Destroy(checkPoint);
         }
     }
