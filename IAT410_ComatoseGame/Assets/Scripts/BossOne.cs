07980e530bfea7bd338779ecd590217f9017e424
errr1n <eteply@telus.net>
2024-02-07 11:27:23 -0800

    no more error

--- IAT410_ComatoseGame/Assets/Scripts/BossOne.cs
+++ IAT410_ComatoseGame/Assets/Scripts/BossOne.cs
@@ -5,126 +5,132 @@ using UnityEngine.AI;
 public class BossOne : MonoBehaviour
 {
 
-    public NavMeshAgent agent;
-    public Transform player, spawnPoint;
-    public LayerMask isGround, isPlayer;
+    //START OF ERINS COMMENTS
+
+//     public NavMeshAgent agent;
+//     public Transform player, spawnPoint;
+//     public LayerMask isGround, isPlayer;
 
   
-    public GameObject projectile;
+//     public GameObject projectile;
 
-    //patrolling 
-    public Vector3 walkPoint;
-    bool walkPointSet;
-    public float walkPointRange;
+//     //patrolling 
+//     public Vector3 walkPoint;
+//     bool walkPointSet;
+//     public float walkPointRange;
 
-    //attacking
-    public float timeBetweenAttacks;
+//     //attacking
+//     public float timeBetweenAttacks;
   
-    bool alreadyAttacked;
+//     bool alreadyAttacked;
 
-    //states
-    public float sightRange, attackRange, health;
-    public bool playerInSightRange,playerInAttackRange;
+//     //states
+//     public float sightRange, attackRange, health;
+//     public bool playerInSightRange,playerInAttackRange;
 
-    //projectile boss attacks
-    [SerializeField] private int numberOfProjectiles;
-    [SerializeField] private float radius=5f;
-     [SerializeField] private float moveSpeed=5f;
-    Vector3 startPoint;
+//     //projectile boss attacks
+//     [SerializeField] private int numberOfProjectiles;
+//     [SerializeField] private float radius=5f;
+//      [SerializeField] private float moveSpeed=5f;
+//     Vector3 startPoint;
 
 
-    private void Awake()
-    {
-        player = GameObject.Find("PlayerArmature").transform;
-        agent = GetComponent<NavMeshAgent>();
-    }
+//     private void Awake()
+//     {
+//         player = GameObject.Find("PlayerArmature").transform;
+//         agent = GetComponent<NavMeshAgent>();
+//     }
 
-    private void Patrolling()
-    {
-        if(!walkPointSet) SearchWalkPoint();
+//     private void Patrolling()
+//     {
+//         if(!walkPointSet) SearchWalkPoint();
 
-        if(walkPointSet) agent.SetDestination(walkPoint);
+//         if(walkPointSet) agent.SetDestination(walkPoint);
         
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+//         Vector3 distanceToWalkPoint = transform.position - walkPoint;
 
-        //walkpoint reached
-        if(distanceToWalkPoint.magnitude <1f)
-            walkPointSet = false;
+//         //walkpoint reached
+//         if(distanceToWalkPoint.magnitude <1f)
+//             walkPointSet = false;
             
-    }
+//     }
 
-    private void SearchWalkPoint()
-    {
-        //random point in range
-        float randomZ = Random.Range(-walkPointRange, walkPointRange);
-        float randomX = Random.Range(-walkPointRange, walkPointRange);
+//     private void SearchWalkPoint()
+//     {
+//         //random point in range
+//         float randomZ = Random.Range(-walkPointRange, walkPointRange);
+//         float randomX = Random.Range(-walkPointRange, walkPointRange);
 
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+//         walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
 
-        if(Physics.Raycast(walkPoint, -transform.up, 2f, isGround))
-        {
-            walkPointSet = true;
-        }
-    }
+//         if(Physics.Raycast(walkPoint, -transform.up, 2f, isGround))
+//         {
+//             walkPointSet = true;
+//         }
+//     }
 
-    private void ChasePlayer() 
-    {
-        agent.SetDestination(player.position);
-    }
+//     private void ChasePlayer() 
+//     {
+//         agent.SetDestination(player.position);
+//     }
 
-    private void AttackPlayer()
-    {
-        //stop enemy from moving 
-        agent.SetDestination(transform.position);
-        transform.LookAt(player);
+//     private void AttackPlayer()
+//     {
+//         //stop enemy from moving 
+//         agent.SetDestination(transform.position);
+//         transform.LookAt(player);
 
-        if(!alreadyAttacked)
-        {
-            //original projectile
-            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
-            rb.AddForce(transform.forward *32f, ForceMode.Impulse);
-            rb.AddForce(transform.up *5f, ForceMode.Impulse);
+//         if(!alreadyAttacked)
+//         {
+//             //original projectile
+//             Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+//             rb.AddForce(transform.forward *32f, ForceMode.Impulse);
+//             rb.AddForce(transform.up *5f, ForceMode.Impulse);
             
-            //spread stuff
-           Rigidbody rb2 = Instantiate(projectile, transform.position.x+100, Quaternion.identity).GetComponent<Rigidbody>();
-            rb2.AddForce(transform.forward *32f, ForceMode.Impulse);
-            rb2.AddForce(transform.up *5f, ForceMode.Impulse);
+//             //spread stuff
+//            Rigidbody rb2 = Instantiate(projectile, transform.position.x+100, Quaternion.identity).GetComponent<Rigidbody>();
+//             rb2.AddForce(transform.forward *32f, ForceMode.Impulse);
+//             rb2.AddForce(transform.up *5f, ForceMode.Impulse);
             
-            alreadyAttacked = true;
-            Invoke(nameof(ResetAttack), timeBetweenAttacks);
-        } 
+//             alreadyAttacked = true;
+//             Invoke(nameof(ResetAttack), timeBetweenAttacks);
+//         } 
    
-    }
+//     }
    
 
 
-    private void ResetAttack() 
-    {
-        alreadyAttacked = false;
-    }
+//     private void ResetAttack() 
+//     {
+//         alreadyAttacked = false;
+//     }
    
-   public void TakeDamage(int damage)
-   {
-        health -= damage;
+//    public void TakeDamage(int damage)
+//    {
+//         health -= damage;
 
-        if(health<=0 ) Invoke(nameof(DestroyEnemy), 3f);
-   }
+//         if(health<=0 ) Invoke(nameof(DestroyEnemy), 3f);
+//    }
 
-   public void DestroyEnemy(){
-        Destroy(gameObject);
-   }
-    // Update is called once per frame
-    void Update()
-    {
-        //checking for sight and attack range
-        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, isPlayer);
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, isPlayer);
+//    public void DestroyEnemy(){
+//         Destroy(gameObject);
+//    }
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         //checking for sight and attack range
+//         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, isPlayer);
+//         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, isPlayer);
 
-        //enemy patrols if not in attack or sight range of player
-        if(!playerInSightRange && !playerInAttackRange) Patrolling();
-        if(playerInSightRange && !playerInAttackRange) ChasePlayer();
-        if(playerInSightRange && playerInAttackRange) AttackPlayer();
-    }
+//         //enemy patrols if not in attack or sight range of player
+//         if(!playerInSightRange && !playerInAttackRange) Patrolling();
+//         if(playerInSightRange && !playerInAttackRange) ChasePlayer();
+//         if(playerInSightRange && playerInAttackRange) AttackPlayer();
+//     }
+
+
+
+    //END OF ERINS COMMENTS
 
     
     // public void OnCollisionEnter(Collision collision){
