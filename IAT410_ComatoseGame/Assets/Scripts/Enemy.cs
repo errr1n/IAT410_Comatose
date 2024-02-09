94461d12dc9eeb20cd2871cd3b13550ac1e76ca1
Anika Richards <109831070+anika-26@users.noreply.github.com>
2024-02-08 22:08:21 -0800

    enemy healthbars

--- IAT410_ComatoseGame/Assets/Scripts/Enemy.cs
+++ IAT410_ComatoseGame/Assets/Scripts/Enemy.cs
@@ -1,23 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
-    public float health;
+    public float health, maxHealth, curHealth;
     public GameObject healthPack;
+    public Slider healthBar;
    
     // Start is called before the first frame update
-    void Start()
-    {
-       health = 3;
+    // void Start()
+    // {
+    //    health = 3;
+    // }
+    void Start(){
+         curHealth = maxHealth;
+        healthBar.value = curHealth;
+        healthBar.maxValue = maxHealth;
     }
+    void Awake()
+    {
+        curHealth = maxHealth;
+        healthBar.value = curHealth;
+        healthBar.maxValue = maxHealth;
+
+    }
+
+    public void sendDamage (float damageValue){
+        Debug.Log("dmg : " + damageValue);
+        curHealth -= damageValue;
+        healthBar.value = curHealth;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
         
-        if(health <=0){
+        if(curHealth <=0){
             Destroy(gameObject);
 
             // here would generate a health pack
@@ -32,6 +53,7 @@ public class Enemy : MonoBehaviour
             //other.GetComponent<player>().Damage();
             Debug.Log("hit enemy");
             health-=1;
+            sendDamage(10);
            // Destroy(gameObject);
             //need to destroy player projectile too 
            // this.GetComponent<AINavPath>().TakeDamage(1);
