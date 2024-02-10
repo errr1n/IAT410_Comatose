27b161c7ef7b1fadfc1d628648125b3d11113d4c
Anika Richards <109831070+anika-26@users.noreply.github.com>
2024-02-10 13:49:18 -0800

    checkpoints and checkpoint alert

--- IAT410_ComatoseGame/Assets/Scripts/PlayerCheckpoint.cs
+++ IAT410_ComatoseGame/Assets/Scripts/PlayerCheckpoint.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 //PLAYER CHECKPOINTS THIS IS THE ACTUAL CODE DONT BE SILLY 
 public class PlayerCheckPoint : MonoBehaviour
@@ -8,11 +9,18 @@ public class PlayerCheckPoint : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private  List<GameObject> checkPoints;
     [SerializeField] private Vector3 spawnPoint;
+
+    [SerializeField] private TMP_Text checkpointText;
+
+    private int duration = 3;
+    private int timeRemaining;
+    private bool isCountingDown = false;
   
     //test to define first respawn point
     void Start() 
     {
         spawnPoint = new Vector3(122.1f, 6.24f, 87.56f);
+        checkpointText.enabled = false;
     }
 
     // Update is called once per frame
@@ -31,12 +39,29 @@ public class PlayerCheckPoint : MonoBehaviour
         if(other.gameObject.CompareTag("Checkpoint"))
         {
             Debug.Log("Checkpoint");
-            
+            checkpointText.enabled = true;
+            if(!isCountingDown)
+            {
+                timeRemaining = duration;
+                Invoke("Tick",1f);
+            }
             spawnPoint = other.transform.position;
             Destroy(other.gameObject);
             //Destroy(checkPoint);
         }
     }
+
+    private void Tick() 
+    {
+        timeRemaining--;
+        if(timeRemaining >0) {
+            Invoke("Tick", 1f);
+        } else {
+            isCountingDown = false;
+            checkpointText.enabled = false;
+        }
+    }
+   
 }
 
 
