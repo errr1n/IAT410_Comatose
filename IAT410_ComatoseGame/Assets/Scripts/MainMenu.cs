b338f31c8cad3e4597a10664fd682b0466721e6a
errr1n <eteply@telus.net>
2024-02-11 11:43:36 -0800

    fixed mouse

--- IAT410_ComatoseGame/Assets/Scripts/MainMenu.cs
+++ IAT410_ComatoseGame/Assets/Scripts/MainMenu.cs
@@ -8,6 +8,8 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
